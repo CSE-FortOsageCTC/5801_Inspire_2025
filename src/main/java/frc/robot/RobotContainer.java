9a857b341452aab585178762468b96962931fb87
mxwell <m.row7910@gmail.com>
2025-01-15 17:33:50 -0600

    led subsystem added, hopefully works!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,13 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,11 +24,14 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private LEDSubsystem s_LEDSubsystem = LEDSubsystem.getInstance();
+
+  private final Joystick driver = new Joystick(0);
+  private final Joystick operator = new Joystick(1);
+
+  private final JoystickButton ledCheck = new JoystickButton(operator, XboxController.Button.kA.value);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,12 +50,11 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    ledCheck.onTrue(new InstantCommand(() -> s_LEDSubsystem.setLEDs()));
   }
 
   /**
@@ -58,6 +64,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
+import edu.wpi.first.wpilibj.PWM;
+
+public class LEDSubsystem extends SubsystemBase {
+
+  private final PWM pwm;
+  private static LEDSubsystem ledSubsystem;
+
+  /** Creates a new ExampleSubsystem. */
+  public LEDSubsystem() {  
+    pwm = new PWM(1);
+  }
+
+  public void setLEDs () {
+    Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
+
+    // pwm.setPulseTimeMicroseconds(2125);
+    pwm.setSpeed(0.99);
+
+    if(alliance.get().equals(Alliance.Blue)){
+      pwm.setSpeed(0.87);
+    } else { 
+      pwm.setSpeed(0.61);
+    }
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  public static LEDSubsystem getInstance() {
+    if (ledSubsystem == null) {
+        ledSubsystem = new LEDSubsystem();
+    }
+    return ledSubsystem;
+}
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
